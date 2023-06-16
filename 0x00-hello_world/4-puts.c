d681395c3b20d216fa7597e58c065e5cf88915da
Pa-vic <victorndudim2000@gmail.com>
2023-06-15 20:06:04 -0700

    commited again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point of the program
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	/* 
-	 * Entry point of the program
-	 */
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
