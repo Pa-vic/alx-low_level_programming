d8e27416f841c842287fdb5bc64f85f0ad3306ef
Pa-vic <victorndudim2000@gmail.com>
2023-06-20 15:09:17 -0700

    num 1 again

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ */
+void print_alphabet(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
