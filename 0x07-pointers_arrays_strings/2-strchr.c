caef6b12e39cdd60e456a14d528d2e343588e672
Pa-vic <victorndudim2000@gmail.com>
2023-07-03 16:24:28 -0700

    a function that checks for a character - task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,18 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * _strchr - a function to locate a character
- * @s: pointer string
- * @c: character to out for
- * Return: s at the first occurance of character else NULL
+ * _strchr - locates a character in a string,
+ * @s: string.
+ * @c: character.
+ * Return: the pointer to the first occurrence of the character c.
  */
 char *_strchr(char *s, char c)
 {
 	unsigned int a = 0;
 
-	while (*(s + a) != '\0')
-	{
+	for (; *(s + a) != '\0'; a++)
+		if (*(s + a) == c)
+			return (s + a);
 	if (*(s + a) == c)
 		return (s + a);
-	a++;
-	}
-	if (*(s + a) == c)
-		return (s + a);
-	return (\0);
+	return ('\0');
 }
