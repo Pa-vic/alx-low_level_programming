dce0099d1b3412ad53592b280ae50911866076ef
Pa-vic <victorndudim2000@gmail.com>
2023-07-14 13:08:54 -0700

    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-#define ERR_MSG "Error"
 
 /**
  * _is_zero - determines if any number is zero
@@ -12,10 +11,10 @@
  */
 void _is_zero(char *argv[])
 {
-	int j, i, isn1 = 1, isn2 = 1;
+	int i, isn1 = 1, isn2 = 1;
 
-	for (j = 0; argv[1][j]; j++)
-		if (argv[1][j] != '0')
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
 		{
 			isn1 = 0;
 			break;
@@ -84,29 +83,29 @@ int _checknum(char *argv[], int n)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, lnout, mul, addl, i, j, k, ca;
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
 	char *nout;
 
 	if (argc != 3)
 		printf("Error\n"), exit(98);
-	num1 = _checknum(argv, 1), num2 = _checknum(argv, 2);
-	_is_zero(argv), lnout = num1 + num2, nout = malloc(lnout + 1);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
 	nout = _initialize_array(nout, lnout);
-	k = lnout - 1, i = num1 - 1, j = num2 - 1, ca = addl = 0;
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
 		if (i < 0)
 		{
 			if (addl > 0)
 			{
-				mul = (nout[k] - '0') + addl;
-				if (mul > 9)
-					nout[k - 1] = (mul / 10) + '0';
-				nout[k] = (mul % 10) + '0';
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
 			}
-			i = num1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
 		}
 		if (j < 0)
 		{
@@ -114,12 +113,12 @@ int main(int argc, char *argv[])
 				break;
 			lnout--;
 			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
-			k = lnout - 1, i = num1 - 1, j = num2 - 1, ca = addl = 0;
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 		}
 		if (j >= 0)
 		{
-			mul = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
-			addl = mul / 10, nout[k] = (mul % 10) + '0';
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
 		}
 	}
 	printf("%s\n", nout);
